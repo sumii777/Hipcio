35c333b539b051ea10b0dcadbfb0539b08389a9b
Igor Burian <proqramista@gmail.com>
2025-11-03 11:22:51 +0100

    usnięcie wszsystkiego

--- Form1.cs
+++ Form1.cs
@@ -17,17 +17,7 @@ namespace Hipcio
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
