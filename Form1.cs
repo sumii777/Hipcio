7cd9f0fd29682588db92abbb65300b243558cdc5
Igor Burian <proqramista@gmail.com>
2025-11-03 11:01:41 +0100

    dodanie przycisku

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace Hipcio
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
