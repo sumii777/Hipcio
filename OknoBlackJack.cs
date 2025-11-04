acbe20292b8a2c1aa213fcd671073f4ef4631e8c
Igor Burian <proqramista@gmail.com>
2025-11-04 16:10:19 +0100

    dodanie okien wszystkich gier

---
+++ OknoBlackJack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hipcio
+{
+    public partial class OknoBlackJack : Form
+    {
+        public OknoBlackJack()
+        {
+            InitializeComponent();
+        }
+    }
+}
